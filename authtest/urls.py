412c76a5ff815563079ba5496a8fec73e9b7eee6
1f10210384 <s1f102103842@iniad.org>
2022-12-22 16:10:31 +0900

    url/edit

--- authtest/urls.py
+++ authtest/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('priv', views.private_page, name='priv'),
     path('pub', views.public_page, name='pub'),
+
+
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:article_id>/delete', views.delete, name='delete'),
+    path('<int:article_id>/update', views.update, name='update'),
+    path('<int:article_id>/like', views.like, name='like'),
+    path('api/articles/<int:article_id>/like', views.api_like),
 ]
