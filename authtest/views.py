ae4db0f2fdcb49e804bb1a71269cf32bc7d38005
1f10210384 <s1f102103842@iniad.org>
2022-12-22 15:23:10 +0900

    pub/pri

--- authtest/views.py
+++ authtest/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.http import Http404, JsonResponse
+from django.utils import timezone
+import random
+from authtest.models import Article, Comment
 
 # Create your views here.
 def home(request):
@@ -11,3 +16,89 @@ def private_page(request):
 
 def public_page(request):
     return render(request, 'authtest/public.html', {})
+
+def index(request):
+    if request.method == 'POST':
+        article = Article(title=request.POST['title'], body=request.POST['text'])
+        article.save()
+        return redirect(detail, article.id)
+    
+    if ('sort' in request.GET):
+        if request.GET['sort'] == 'like':
+            articles = Article.objects.order_by('-like')
+        else:
+            articles = Article.objects.order_by('-posted_at')
+    else:
+        articles = Article.objects.order_by('-posted_at')
+    context = {
+        "articles": articles
+    }
+    return render(request, 'team9app/index.html', context)
+
+def update(request, article_id):
+    return HttpResponse("article_id: {}".format(article_id))
+
+def detail(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    
+    if request.method == 'POST':
+        comment = Comment(article=article, text=request.POST['text'])
+        comment.save()
+
+    context = {
+        "article": article,
+        'comments': article.comments.order_by('-posted_at')
+    }
+    return render(request, "team9app/detail.html", context)
+
+def update(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    if request.method == 'POST':
+        article.title = request.POST['title']
+        article.body = request.POST['text']
+        article.save()
+        return redirect(detail, article_id)
+    context = {
+        "article": article
+    }
+    return render(request, "team9app/edit.html", context)
+
+def delete(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    
+    article.delete()
+    
+    return redirect(index)
+
+def like(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+        article.like += 1
+        article.save()
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    
+    return redirect(detail, article_id)
+
+def api_like(request, article_id):
+    try:
+        article = Article.objects.get(pk=article_id)
+        article.like += 1
+        article.save()
+    except Article.DoesNotExist:
+        raise Http404("Article does not exist")
+    
+    result = {
+        'id' : article_id,
+        'like' : article.like
+    }
+    return JsonResponse(result)
