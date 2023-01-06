73646f3d90693d9fb5c7ad27afc2b7f895d4325c
1f10210384 <s1f102103842@iniad.org>
2023-01-06 18:17:09 +0900

    stylechange

--- authtest/urls.py
+++ authtest/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('priv', views.private_page, name='priv'),
     path('pub', views.public_page, name='pub'),
-
-
     path('<int:article_id>/', views.detail, name='detail'),
     path('<int:article_id>/delete', views.delete, name='delete'),
     path('<int:article_id>/update', views.update, name='update'),
